5e6cb2ac4736f7b9df7ab35988d333906c69e2ff
Jobisss <jgpschianti07@gmail.com>
2025-07-30 21:03:53 -0300

    fisrt commit-

---
+++ src/main/java/projeto/lojaTech/LojaTechApplication.java
@@ -0,0 +1,13 @@
+package projeto.lojaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaTechApplication.class, args);
+	}
+
+}
