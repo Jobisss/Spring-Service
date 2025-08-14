6c65644773b3f957504d77adc276b747c55fd384
João Gabriel <jgpschianti07@gmail.com>
2025-08-13 22:10:19 -0300

    start: loja tech hibernate

--- src/main/java/projeto/lojaTech/LojaTechApplication.java
+++ src/main/java/projeto/lojaTech/LojaTechApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LojaTechApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LojaTechApplication.class, args);
 	}
